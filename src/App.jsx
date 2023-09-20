6f853784f5480b11b972cc8a812063be495cb558
astarta16 <ngedeshuri2001@gmail.com>
2023-09-20 12:11:48 +0400

    create starter files

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Image from './assets/illustration-woman-online-mobile.svg'
+
+function App() {
+  return (
+   
+    <div className="w-[920px] h-[509px] bg-white">
+      <div>
+        <img src={Image} />
+      </div>
+
+    </div>
+
+    
+  );
+ 
+}
+
+export default App
