05b37688f1652947178d5c1308ad449b2f041fb6
astarta16 <ngedeshuri2001@gmail.com>
2023-09-20 18:33:51 +0400

    add some styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,27 @@
 import { useState } from 'react';
 import ImageMobile from './assets/illustration-woman-online-mobile.svg'
-import ImageDesk from './assets/illustration-woman-online-desktop.svg'
-
-import Questions from './components/data';
+// import ImageDeskop from './assets/illustration-woman-online-desktop.svg'
+// import data from './components/data'
 
 function App() {
   return (
-    <div>
-     
+    <div className="flex justify-center items-center min-h-screen ">
+      <div className="bg-white shadow-md rounded-lg text-center w-[920px] h-[509px]">
+         <h1 className="text-black text-2xl ml-[100px] font-semibold">FAQ</h1>
+         <div className="w-1/2">
+          <img
+          src={ImageMobile}
+          alt="Card Image"
+         className='top-[-40px]'
+          
+        />
+         </div>
+        
+       
+       
+      </div>
     </div>
-  )
-  
-    
-
-    
-  
+  );
 }
 
 export default App
